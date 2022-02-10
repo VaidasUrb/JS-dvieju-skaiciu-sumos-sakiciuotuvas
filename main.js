fb1d2ddbac921b430d05ba397bbddb21d5329dc7
VaidasUrb <vaidurbon@yahoo.com>
2022-02-10 20:58:44 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let forma = document.querySelector("form");
+let rezultatas = document.createElement("p");
+
+forma.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let number1 = +forma["number1"].value;
+    let number2 = +forma["number2"].value;
+    sum = number1 + number2;
+    if (sum == sum) {
+        rezultatas.textContent = `Atsakymas ${sum} `;
+    } else {
+        rezultatas.textContent = "nesamone";
+    }
+    body.appendChild(rezultatas);
+})
