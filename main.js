5cbc8468bdb2468a3d815337475e56a2bc5a9add
VaidasUrb <vaidurbon@yahoo.com>
2022-02-10 22:09:37 +0200

    ???

--- main.js
+++ main.js
@@ -13,3 +13,36 @@ forma.addEventListener("submit", function (e) {
     }
     body.appendChild(rezultatas);
 })
+
+
+let forma2 = document.getElementsByName("form")[1];
+let apskaiciavimas = document.createElement("p");
+let content = document.querySelector("body");
+
+forma2.addEventListener("submit", submitInputs);
+function ubmitInputs(e) {
+    e.preventDefault();
+    let svoris = +forma2[number3].value;
+    let ugis = +forma2[number4].value;
+    displayResult(calcBMI(svoris, ugis));
+}
+
+function displayResult(bmi) {
+    XPathResult.textContent = `jusu kmi: ${bmi}, busena ${checkWeightStatus(bmi)}.`;
+
+}
+function calcBMI(kg, m) {
+    return (kg / (m * m)).toFixed(2);
+}
+function checkWeightStatus(kmi) {
+    let result;
+    if (kmi < 18.5) {
+        result = "mazas svoris";
+    } else if (18.5 <= kmi && kmi < 25) {
+        result = "vid svoris";
+    } else if (25 <= kmi && kmi < 30) {
+        result = "vissvoris";
+    } else {
+        result = "nutukimas";
+    }
+}
